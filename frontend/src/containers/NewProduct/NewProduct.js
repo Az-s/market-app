41305c71041521f27132c4f7b0ddc2327893e5ea
Az-s <azim.sa@yahoo.com>
2021-11-20 15:42:33 +0600

    created newProduct

--- frontend/src/containers/NewProduct/NewProduct.js
+++ frontend/src/containers/NewProduct/NewProduct.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import ProductForm from '../../components/ProductForm/ProductForm';
+import { useDispatch } from "react-redux";
 
-const NewProduct = () => {
+const NewProduct = ({history}) => {
+    const dispatch = useDispatch();
+
+    // const onSubmit = async messageData => {
+    //     await dispatch(createMessage(messageData));
+    //     history.replace('/');
+    // };
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Typography variant="h4" sx={{ margin: '1rem 0' }}>New message</Typography>
+            <ProductForm
+                onSubmit={onSubmit}
+            />
+        </>
     )
 }
 
--- frontend/src/containers/Product/Product.js
+++ frontend/src/containers/Product/Product.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Grid, Card, CardMedia, CardHeader, CardContent, Button, Typography, CardActions } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useDispatch, useSelector } from "react-redux";
 
 const Product = () => {
     return (
-        <div>
-            
-        </div>
+        <Grid container justifyContent='center'>
+            <Card sx={{ minWidth: 600, margin: '1rem' }}>
+                <CardHeader
+                    title="MacBook"
+                    subheader="category"
+                />
+                <CardMedia
+                    component="img"
+                    height="300"
+                    image='https://images.pexels.com/photos/441963/pexels-photo-441963.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+                    alt="album"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" >
+                        Description about product
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" >
+                        John Doe + 77 77 77
+                    </Typography>
+                </CardContent>
+                <CardActions sx={{ justifyContent: 'center' }}>
+                    <Button variant="outlined" startIcon={<DeleteIcon />} color="error">
+                        Delete
+                    </Button>
+                </CardActions>
+            </Card>
+        </Grid>
     )
 }
 
