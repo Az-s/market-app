97d719498b0f935feb835748279f416a318df15a
Az-s <azim.sa@yahoo.com>
2021-11-20 19:40:38 +0600

    add preloader

--- frontend/src/components/UI/NavBar/NavBar.js
+++ frontend/src/components/UI/NavBar/NavBar.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Button , Grid } from '@mui/material';
 import { Link } from "react-router-dom";
+import {useSelector} from "react-redux";
+import UserMenu from './Menu/UserMenu';
+import AnonymousMenu from './Menu/AnonymousMenu';
 
 const NavBar = () => {
+    const user = useSelector(state => state.users.user);
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -10,7 +15,14 @@ const NavBar = () => {
                     <Typography variant="h6" component={Link} to="/" sx={{ color: 'white', textDecoration: 'none', flexGrow: 1 }}>
                         Market App
                     </Typography>
-                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                    <Grid item>
+                        {user ? (
+                            <UserMenu user={user} />
+                        ) : (
+                            <AnonymousMenu />
+                        )}
+                    </Grid>
+                    {/* <Button color="inherit" component={Link} to="/login">Login</Button> */}
                 </Toolbar>
             </AppBar>
         </Box>
