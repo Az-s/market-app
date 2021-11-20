e8823fbf609659806d00b769e3e7ad623fe0eeb0
Az-s <azim.sa@yahoo.com>
2021-11-20 15:06:46 +0600

    created Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import NavBar from './components/UI/NavBar/NavBar';
+import Products from "./containers/Products/Products";
 import './App.css';
+import Login from "./containers/Login/Login";
+import Register from "./containers/Register/Register";
+import Product from "./containers/Product/Product";
 
 const App = () => {
   return (
     <div className="App">
-
+      <Router>
+        <NavBar />
+        <Route exact path="/" component={Products} />
+        <Route path="/product/:id" component={Product} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        {/* <Route path="/albums" component={AlbumList} />
+        <Route path="/album/:id" component={AlbumInfo} /> */}
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/UI/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar variant="dense" sx={{ justifyContent: 'center' }}>
+                    <Typography variant="h6" component={Link} to="/" sx={{ color: 'white', textDecoration: 'none', flexGrow: 1 }}>
+                        Market App
+                    </Typography>
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default NavBar
